7f5e1ec5d5312b25c12baffee46c17341d084b13
mdyasinarif <mdyasinarifidb49@gmail.com>
2019-08-04 12:50:19 +0600

    my resume

---
+++ yasinarif/src/main/java/com/yasinarif/YasinarifApplication.java
@@ -0,0 +1,13 @@
+package com.yasinarif;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YasinarifApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YasinarifApplication.class, args);
+    }
+
+}
